68e6d6dafea54f4760faebde6781f15444549780
aravindpadman <aravindpadman@gmail.com>
2020-10-03 10:48:43 +0000

    partially working pipeline

--- cuml_lr_with_targetencoding.py
+++ cuml_lr_with_targetencoding.py
@@ -19,7 +19,7 @@ from config import Config
 import cudf
 import cuml
 from cuml.preprocessing import OneHotEncoder, TargetEncoder
-from cuml.linear_model import logistic_regression
+from cuml.linear_model import LogisticRegression
 
 
 np.random.seed(Config.RANDOM_SEED)
@@ -52,7 +52,6 @@ class OHEColumnTransform(BaseEstimator, TransformerMixin):
 class TargetEncodeTransform(BaseEstimator, TransformerMixin):
     def __init__(self, columns, n_folds=4, smooth=0, seed=42, split_method='interleaved', output_type='auto'):
         self.columns= columns
-        self.kwargs = kwargs
         self.te = None
         self.n_folds = n_folds
         self.smooth = smooth
@@ -67,24 +66,32 @@ class TargetEncodeTransform(BaseEstimator, TransformerMixin):
     
     def transform(self, X, y=None):
         if self.te:
-            X_transformed = X.copy()
+            X_transformed = X.copy().reset_index(drop=True)
             cp_ohe = self.te.transform(X_transformed[self.columns])
-            temp = cudf.DataFrame(cp_ohe, index= X_transformed.index, columns = ['te_'+str(i) for i in range(self.columns)])
+            temp = cudf.DataFrame(cp_ohe, index= X_transformed.index, columns = ['targetencode_'+ "_".join(self.columns)])
             X_transformed = X_transformed.drop(self.columns, axis=1)
             X_transformed = X_transformed.join(temp)
             return X_transformed
         else:
             raise("Target encoding must fit before transform")
+    # def fit_transform(self, X, y, **fit_params):
+    #     return self.fit(X, y).transform(X)
 
 if __name__ == "__main__":
-    train = cudf.read_csv("/kaggle/input/cat-in-the-dat/train.csv", index_col='id')
+    train = cudf.read_csv("/kaggle/input/cat-in-the-dat/train.csv", index_col=None)
     test =  cudf.read_csv("/kaggle/input/cat-in-the-dat/test.csv", index_col='id')
     submission = cudf.read_csv("/kaggle/input/cat-in-the-dat/sample_submission.csv", index_col='id')
-    
-    columns = ['bin_0', 'nom_4']
 
-    kwargs={'handle_unknown': 'ignore', 'sparse': False}
-    ohe = OHEColumnTransform(columns, handle_unknown='ignore', sparse=False)
-    ohe.fit(train)
-    temp = ohe.transform(train)
+    X = train.drop('target', axis=1)
+    y = train['target']
+    
+    one_hot_columns = ['bin_0', 'bin_1', 'bin_2', 'bin_3', 'bin_4', 'nom_0', 'nom_1', 'nom_2', 'nom_3', 'nom_4']
+    pipe = make_pipeline(
+        OHEColumnTransform(one_hot_columns, handle_unknown='ignore', sparse=False),
+        TargetEncodeTransform(columns=['nom_9', 'nom_8']),
+        None,
+    )
+    pipe = pipe.fit(X, y)
+    X_transformed = pipe.transform(X)
+    print(X_transformed.head())
     print("hello")
